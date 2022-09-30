fc42de00baf2774a40e2eea74ba7783ed104b6af
Artem Lizantan <plishkan25@gmail.com>
2022-09-30 13:59:20 +0300

    fix

--- src/js/modules/first-page/heroSwiper.js
+++ src/js/modules/first-page/heroSwiper.js
@@ -3,3 +3,25 @@ import Swiper, {
     Pagination
 } from 'swiper';
 
+
+function hotelSwiper(){
+
+    const swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        keyboard: {
+            enabled: true,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+}
+
+
+hotelSwiper()
